edb11eae05cae261352c7e9121bed7883187885e
hum <wawe8963098@163.com>
2021-07-09 15:37:55 +0800

    test param

--- src/main/java/com/aliyun/adb/contest/constants/Constants.java
+++ src/main/java/com/aliyun/adb/contest/constants/Constants.java
@@ -10,7 +10,7 @@ public interface Constants {
     int SEGMENT_MARGE = 50;
     int SEGMENT_SIZE = 512 * 1024 - SEGMENT_MARGE;
 
-    int READ_NUM_CORE = 4;
+    int READ_NUM_CORE = 8;
 //    int READ_BUFFER_NUM = 1 << 6;
 
     int WRITE_BUFFER_SIZE = 64 * 1024;
@@ -22,7 +22,7 @@ public interface Constants {
 //    int BUCKET_MEMO_MARGE = 50000;
 //    int BUCKET_MEMO_SIZE = 3600000 / READ_NUM_CORE + BUCKET_MEMO_MARGE;
 
-    int COMPUTE_NUM_CORE = 4;
+    int COMPUTE_NUM_CORE = 8;
 
     // local 50000000 ,  prod  1000000000;
     int RECORD_SUM = 1000000000;
