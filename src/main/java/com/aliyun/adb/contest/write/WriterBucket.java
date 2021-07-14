f82bd5736863bab27c1833b0f820c7205cb5a3f0
hum <wawe8963098@163.com>
2021-07-14 16:57:10 +0800

    bufnum

--- src/main/java/com/aliyun/adb/contest/write/WriterBucket.java
+++ src/main/java/com/aliyun/adb/contest/write/WriterBucket.java
@@ -15,7 +15,7 @@ import java.util.concurrent.Future;
 public class WriterBucket {
     private final FileChannel fileChannel;
 
-    private final int bufNum = 3;
+    private final int bufNum = 2;
 
     private final ByteBuffer[] buffers = new ByteBuffer[bufNum];
     private final Future[] futures = new Future[bufNum];
@@ -36,7 +36,7 @@ public class WriterBucket {
     public void putMessage(byte[] message, int startPosition, int pos) {
         if (!buffers[index].hasRemaining()) {
             ByteBuffer tmpBuffer = buffers[index];
-            int newIndex = (index + 1) % bufNum;
+            int newIndex = (index + 1) & 1;
             tmpBuffer.flip();
             if (!futures[newIndex].isDone()) {
                 System.out.println("data block");
